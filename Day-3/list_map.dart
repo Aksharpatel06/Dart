783ca29f5fe66f23394df324e106c6daf09cb81e
Akshar Patel <143181114+Aksharpatel06@users.noreply.github.com>
2023-12-24 11:30:37 +0530

    Add files via upload

---
+++ Day-3/list_map.dart
@@ -0,0 +1,8 @@
+void main() {
+  var list1 = {10, 20, "hello", 2.5, 'w'};
+  print(list1);
+
+  var details = {'Usrname': 'tom', 'Password': 'pass@123'};
+  details['Uid'] = 'U1oo1';
+  print(details);
+}
