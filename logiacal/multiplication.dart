3a07477a8ecd3d7b1c8429b64c32ed69d16f59b1
Akshar Patel <143181114+Aksharpatel06@users.noreply.github.com>
2024-01-08 10:24:10 +0530

    Delete logiacal directory

--- logiacal/multiplication.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-void main() {
-  stdout.write("enter the table number:");
-  int n = int.parse(stdin.readLineSync()!);
-  for (int i = 1; i <= 10; i++) {
-    print("$n * $i = ${i * n}");
-  }
-}
